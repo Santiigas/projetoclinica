fe5aa0fd060637708e0658a144d6ff4948dac842
Santiigas <santiagosof@gmail.com>
2023-08-24 00:10:27 -0300

    +

---
+++ teste2.js
@@ -0,0 +1,15 @@
+const data = "16022003"
+
+function VerificaData(data){
+    mes = data[2] + data[3]
+    mes31 = []
+    console.log(contanecao)
+}
+
+VerificaData(data)
+
+
+
+//verificar o mes
+// 30 31 28 - 3 possibilidades
+//
