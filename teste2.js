3b7671150f24c71d4000b32972e953aa3b9ef69f
Santiigas <santiagosof@gmail.com>
2023-08-24 23:47:55 -0300

    +

--- teste2.js
+++ teste2.js
@@ -1,4 +1,4 @@
-const data = "16122003"
+const data = "16012003"
 
 function VerificaData(data){
     var mes = data[2] + data[3]
@@ -8,31 +8,6 @@ function VerificaData(data){
     } else{
         console.log('Invalido')
     }
-    console.log(mes)
 }
 
 VerificaData(data)
-
-
-//verificar o mes
-// 30 31 28 - 3 possibilidades
-//
-
-/*
-const data = "16002003"
-
-function VerificaData(data){
-    var mes = data[2] + data[3]
-    var mesvalue = parseInt(mes)
-    if(mesvalue > 1 && mesvalue <= 12){
-        console.log("Valido")
-    } else{
-        console.log('Invalido')
-    }
-    console.log(mes)
-    console.log(typeof mes)
-    //console.log(meses_do_ano.includes(mes))
-}
-
-VerificaData(data)
-*/
