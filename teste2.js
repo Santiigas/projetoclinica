6b51d833606dfa5305f1d3cb58cb204e5c3b0576
Santiigas <santiagosof@gmail.com>
2023-08-24 23:37:36 -0300

    função de data parte1

--- teste2.js
+++ teste2.js
@@ -1,15 +1,38 @@
-const data = "16022003"
+const data = "16122003"
 
 function VerificaData(data){
-    mes = data[2] + data[3]
-    mes31 = []
-    console.log(contanecao)
+    var mes = data[2] + data[3]
+    var mesvalue = parseInt(mes)
+    if(mesvalue > 1 && mesvalue <= 12){
+        console.log("Valido")
+    } else{
+        console.log('Invalido')
+    }
+    console.log(mes)
 }
 
 VerificaData(data)
 
 
-
 //verificar o mes
 // 30 31 28 - 3 possibilidades
 //
+
+/*
+const data = "16002003"
+
+function VerificaData(data){
+    var mes = data[2] + data[3]
+    var mesvalue = parseInt(mes)
+    if(mesvalue > 1 && mesvalue <= 12){
+        console.log("Valido")
+    } else{
+        console.log('Invalido')
+    }
+    console.log(mes)
+    console.log(typeof mes)
+    //console.log(meses_do_ano.includes(mes))
+}
+
+VerificaData(data)
+*/
