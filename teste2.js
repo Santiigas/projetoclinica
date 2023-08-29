781648c42351ba16178face6634d7ab173ab6239
Santiigas <santiagosof@gmail.com>
2023-08-28 21:44:54 -0300

    +
    função que verifica o mes

--- teste2.js
+++ teste2.js
@@ -1,13 +1,25 @@
-const data = "16012003"
+const data = "01012003"
 
 function VerificaData(data){
     var mes = data[2] + data[3]
     var mesvalue = parseInt(mes)
-    if(mesvalue > 1 && mesvalue <= 12){
-        console.log("Valido")
+    if(mesvalue >= 1 && mesvalue <= 12){
+        console.log("Mes Valido")
     } else{
-        console.log('Invalido')
+        console.log('Mes Invalido')
     }
-}
+
+    var dia = data[0] + data[1]
+    var diavalue = parseInt(dia)
+    if (diavalue >= 1 && diavalue <=31){
+        console.log('Dia valido')
+    } else{
+        console.log('Dia invalido')
+    }
+
+    meses31 = [1, 3, 5, 7, 8, 10, 12]
+    
+
+}   
 
 VerificaData(data)
