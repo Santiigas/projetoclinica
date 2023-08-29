cea6d229251789b872d25d4e4b7b711438d8b95b
Santiigas <santiagosof@gmail.com>
2023-08-28 22:29:51 -0300

    +
    Dia e mês verificado

--- teste2.js
+++ teste2.js
@@ -1,45 +1,33 @@
-const data = "30132003"
+const data = "30022003"
 
 function VerificaData(data){
     var mes = data[2] + data[3]
     var mesvalue = parseInt(mes)
-    if(mesvalue >= 1 && mesvalue <= 12){
-        console.log("Mes Valido")
-    } else{
-        console.log('Mes Invalido')
-    }
-
     var dia = data[0] + data[1]
     var diavalue = parseInt(dia)
-    if (diavalue >= 1 && diavalue <=31){
-        console.log('Dia valido')
-    } else{
-        console.log('Dia invalido')
-    }
-
     meses31 = [1, 3, 5, 7, 8, 10, 12]
     meses30 = [4, 6, 9, 11]
     if(meses31.indexOf(mesvalue) > -1){
-        if(diavalue <= 31){
+        if(diavalue >= 1 && diavalue <=31){
             console.log('Data valida')
         } else{
             console.log('Data invalida')
         }
     } else if(meses30.indexOf(mesvalue) > -1){
-        if(diavalue <= 30){
+        if(diavalue >= 1 && diavalue <=30){
             console.log('Data valida')
         } else{
             console.log('Data invalida')
         }
-    } else {
-        if(diavalue <= 28){
+    } else if(mesvalue === 2){
+        if(diavalue >= 1 && diavalue <=29){
             console.log('Data valida')
         } else{
             console.log('Data invalida')
         }
+    } else{
+        console.log("Data invalida")
     }
-
-
 }   
 
 VerificaData(data)
