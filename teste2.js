cc8ea942e57ccda51ea2c1092d0711423af95115
Santiigas <santiagosof@gmail.com>
2023-08-28 22:09:47 -0300

    +

--- teste2.js
+++ teste2.js
@@ -1,4 +1,4 @@
-const data = "01012003"
+const data = "30132003"
 
 function VerificaData(data){
     var mes = data[2] + data[3]
@@ -18,7 +18,27 @@ function VerificaData(data){
     }
 
     meses31 = [1, 3, 5, 7, 8, 10, 12]
-    
+    meses30 = [4, 6, 9, 11]
+    if(meses31.indexOf(mesvalue) > -1){
+        if(diavalue <= 31){
+            console.log('Data valida')
+        } else{
+            console.log('Data invalida')
+        }
+    } else if(meses30.indexOf(mesvalue) > -1){
+        if(diavalue <= 30){
+            console.log('Data valida')
+        } else{
+            console.log('Data invalida')
+        }
+    } else {
+        if(diavalue <= 28){
+            console.log('Data valida')
+        } else{
+            console.log('Data invalida')
+        }
+    }
+
 
 }   
 
