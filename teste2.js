c26e8a3076fbe4acc0e72943cdf828e0c1d63b2c
Santiigas <santiagosof@gmail.com>
2023-09-04 23:37:14 -0300

    +

--- teste2.js
+++ teste2.js
@@ -1,53 +1,57 @@
-const data = "04322023"
+const data = "04302023"
 
 function VerificaData(data){
-
-    //se dias e meses estão corretos
-    var mes = data[2] + data[3]
-    var mesvalue = parseInt(mes)
+    
     var dia = data[0] + data[1]
     var diavalue = parseInt(dia)
+    var mes = data[2] + data[3]
+    var mesvalue = parseInt(mes)
     var ano = data[4] + data[5] + data[6] + data[7]
     var anovalue = parseInt(ano)
-    meses31 = [1, 3, 5, 7, 8, 10, 12]
-    meses30 = [4, 6, 9, 11]
-    if(meses31.indexOf(mesvalue) > -1){
-        if((diavalue >= 1 && diavalue <=31) === false){
-            console.log('Errado')
-            return false
-        }
-    } else if(meses30.indexOf(mesvalue) > -1){
-        if((diavalue >= 1 && diavalue <=30) === false) {
-            console.log('Errado')
-            return false
-        }
-    } else if(mesvalue === 2){
-        if((diavalue >= 1 && diavalue <=29) === false){
-            console.log('Errado')
-            return false
-        }
-    } 
 
-    //se a data é maior que a atual
-    const date = new Date();
-    var anoatual = date.getFullYear();
-    var diaatual = date.getDate()
-    var mesatual = date.getMonth()
-
-    var dataUsuario = diavalue + mesvalue + anovalue
-    var dataAtual = diaatual + (mesatual + 1) + anoatual
-
-    if(dataUsuario > dataAtual){
-        console.log('Errado')
-        return false
+    function verificaMes(){
+        meses31 = [1, 3, 5, 7, 8, 10, 12]
+        meses30 = [4, 6, 9, 11]
+        if(meses31.indexOf(mesvalue) > -1){
+            if((diavalue >= 1 && diavalue <=31) === false){
+                console.log('Errado')
+                return false
+            }
+        } else if(meses30.indexOf(mesvalue) > -1){
+            if((diavalue >= 1 && diavalue <=30) === false) {
+                console.log('Errado')
+                return false
+            }
+        } else if(mesvalue === 2){
+            if((diavalue >= 1 && diavalue <=29) === false){
+                console.log('Errado')
+                return false
+            }
+        }
+        
+        console.log('Tudo certo') 
     }
-    
-    console.log('Tudo certo')
 
+    function VerificarAno(){
+        const date = new Date();
+        var anoatual = date.getFullYear();
+        var diaatual = date.getDate()
+        var mesatual = date.getMonth()
+
+        var dataUsuario = diavalue + mesvalue + anovalue
+        var dataAtual = diaatual + (mesatual + 1) + anoatual
+
+        if(dataUsuario > dataAtual){
+            console.log('Errado')
+            return false
+        }
+        
+        console.log('Tudo certo')
+    }
 }
 
 
-//VerificaData(data)
+VerificaData(data)
 
 const ano = 2000
 
---
+++ teste3.js
@@ -0,0 +1,22 @@
+
+
+function mae(){
+
+    function filho1(){
+        console.log("filho1")
+    }
+
+    function filho2(){
+        console.log("filho2")
+    }
+
+    function filho3(){
+        console.log("filho3")
+    }
+
+    filho1()
+    filho2()
+    filho3()
+}
+
+mae()
