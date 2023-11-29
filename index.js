832f311f1ddc8e1f54a987644b4b23dae0011dbe
Santiigas <santiagosof@gmail.com>
2023-11-28 23:53:45 -0300

    +

--- index.js
+++ index.js
@@ -1,6 +1,45 @@
 import {validaCPF,validaData,validaNome} from './Modulo.js/validacaodedados.js'
 import {MascaraData, MascaraCPF, MascaraTelefone} from './Modulo.js/mascaras.js'
 
+//informacoes data
+const segunda = document.querySelector()
+const terca = document.querySelector()
+const quarta = document.querySelector()
+const quinta = document.querySelector()
+const sexta = document.querySelector()
+const sabado = document.querySelector()
+
+segunda.addEventListener('click', function(event){
+    const dia = 'Segunda'
+    const horario = '13:00'
+})
+
+terca.addEventListener('click', function(event){
+    const dia = 'Terca'
+    const horario = '13:00'
+})
+
+quarta.addEventListener('click', function(event){
+    const dia = 'Quarta'
+    const horario = '13:00'
+})
+
+quinta.addEventListener('click', function(event){
+    const dia = 'Quinta'
+    const horario = '08:00'
+})
+
+sexta.addEventListener('click', function(event){
+    const dia = 'Sexta'
+    const horario = '08:00'
+})
+
+sabado.addEventListener('click', function(event){
+    const dia = 'Sabado'
+    const horario = '08:00'
+})
+
+
 //informações do paciente
 const form = document.querySelector("#form")
 const nomeInput = document.querySelector('#name')
