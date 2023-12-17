658534ee0f6416f16a0a7eb95de92e6aa7112370
Santiigas <santiagosof@gmail.com>
2023-12-17 16:18:06 -0300

    +

--- teste.js
+++
@@ -1,8 +0,0 @@
-//retorna data atual
-const dataAtual = new Date();
-const dataformatada = dataAtual.toLocaleDateString();
-
-const horas = dataAtual.getHours();
-const minutos = dataAtual.getMinutes();
-const segundos = dataAtual.getSeconds();
-console.log(`${dataformatada}` + " as " + `${horas}` + ":" + `${minutos}` + ":" + `${segundos}`)
